2bda259efbc604973bdf00bedee7f46a76715b73
karllegson <klegson48@gmail.com>
2025-04-24 16:00:48 -0700

    feat(statbar): added statbar

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+lottie.loadAnimation({
+    container: document.getElementById('animation'), // the DOM element
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'lotti.json' // path to your animation JSON
+});
