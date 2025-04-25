ba4459a0a41f7013eea7e0f660bfc4bcf7f4a910
karllegson <klegson48@gmail.com>
2025-04-25 11:45:31 -0700

    feat(statbar): added mobile view responsiveness, and animation update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-lottie.loadAnimation({
-    container: document.getElementById('animation'), 
+window.lottieAnimations = [];
+
+const animation = lottie.loadAnimation({
+    container: document.getElementById('animation'),
     renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'lotti.json'
+    loop: false,         
+    autoplay: false,     
+    path: 'lotti.json'    
 });
+
+animation.setSpeed(0.5);
+
+window.lottieAnimations.push(animation);
